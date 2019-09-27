128f514769ce72c216f356b7057ba6024e5c9d8c
Oghenebrume50 <raphlbrume@gmail.com>
2019-09-27 20:09:18 +0100

    setup scss and css styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import '../styles/css/app.css';
 
 class App extends Component {
   render() {
