f3f765055a3dac2d6b2b7d9233724b3f9fa476af
Oghenebrume50 <raphlbrume@gmail.com>
2019-09-28 10:48:56 +0100

    setup operate.js main functionality

---
+++ src/logic/operate.js
@@ -0,0 +1,24 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operator) => {
+  const first = Big(numberOne);
+  const second = Big(numberTwo);
+
+  if (operator === '+'){
+    return first.plus(second).toString();
+  }
+  else if (operator === '-') {
+    return first.sub(second).toString();
+  }
+  else if (operator === '*') {
+    return first.mul(second).toString();
+  }
+  else if (operator === '/') {
+    return first.div(second).toString();
+  }
+  else if (operator === '%') {
+    return first.mod(second).toString();
+  }
+}
+
+export default operate;
