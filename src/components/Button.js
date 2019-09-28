537f99568745cd94ce4161086b0177ce3faec7fb
Oghenebrume50 <raphlbrume@gmail.com>
2019-09-28 03:19:49 +0100

    finished the styling of the calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 const Button = (props) => {
   return(
-    <div className="button-name">
-      <button>
+    <div className={props.wide ? "wide" : "button" }>
+      <button className={props.orange ? "orange" : "" || props.wide ? "button-wide" : ""}>
         {props.name}
       </button>
     </div>
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,30 +8,30 @@ const ButtonPanel = () => {
         <Button name='AC' />
         <Button name='+/-' />
         <Button name='%' />
-        <Button name='/' />
+        <Button name='/' orange/>
       </div>
       <div>
         <Button name='7' />
         <Button name='8' />
         <Button name='9' />
-        <Button name='*' />
+        <Button name='*' orange/>
       </div>
       <div>
         <Button name='4' />
         <Button name='5' />
         <Button name='6' />
-        <Button name='-' />
+        <Button name='-' orange/>
       </div>
       <div>
         <Button name='1' />
         <Button name='2' />
         <Button name='3' />
-        <Button name='+' />
+        <Button name='+' orange/>
       </div>
       <div>
-        <Button name='0' />
+        <Button name='0' wide/>
         <Button name='.' />
-        <Button name='=' />
+        <Button name='=' orange/>
       </div>
     </div>
   );
