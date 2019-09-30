4108a2e8214862aebeb1f4c6ca39aa52d0fa81bb
Oghenebrume50 <raphlbrume@gmail.com>
2019-09-30 19:21:03 +0100

    made operate.js more readable

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,25 +3,24 @@ import Big from 'big.js';
 const operate = (numberOne, numberTwo, operator) => {
   const first = Big(numberOne);
   const second = Big(numberTwo);
+  
+  let num = '';
+  switch(operator) {
+    case '+': num = first.plus(second).toString()
+      break;
+    case '-': num = first.sub(second).toString();
+      break;
+    case '*': num = first.mul(second).toString();
+      break;
+    case '/': num = first.div(second).toString();
+      break;
+    case '%': num = first.mod(second).toString();
+      break;
+    default: 
+      throw Error(`Unknown operation ${operator}`);
+  }
 
-  if (operator === '+'){
-    return first.plus(second).toString();
-  }
-  else if (operator === '-') {
-    return first.sub(second).toString();
-  }
-  else if (operator === '*') {
-    return first.mul(second).toString();
-  }
-  else if (operator === '/') {
-    return first.div(second).toString();
-  }
-  else if (operator === '%') {
-    return first.mod(second).toString();
-  }
-  else {
-    throw Error(`Unknown operation ${operator}`);
-  }
+  return num;
 }
 
 export default operate;
